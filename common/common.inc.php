50d8bd8d2b42209907e69029f89e448aa22b6939
Felipe Guaycuru <guaycuru@gmail.com>
2017-12-11 11:03:08 -0200

    Desabilita chat ate ele ser migrado

--- common/common.inc.php
+++ common/common.inc.php
@@ -102,7 +102,7 @@ if((!defined('HTML')) || (HTML === true)) {
 	<script type="text/javascript" src="<?= CONFIG_URL; ?>web/js/jquery.watcherkeys.js?<?= REVISION; ?>"></script>
 	<script type="text/javascript" src="<?= CONFIG_URL; ?>web/js/gde.common.js?<?= REVISION; ?>"></script>
 	<script type="text/javascript" src="<?= CONFIG_URL; ?>web/js/gde.42.js?<?= REVISION; ?>"></script>
-    <?php if(($_Usuario !== null) && ($_Usuario->getID() != null) && ((!isset($_SESSION['admin_su'])) || ($_SESSION['admin_su'] === false))) { ?>
+    <?php if(false && ($_Usuario !== null) && ($_Usuario->getID() != null) && ((!isset($_SESSION['admin_su'])) || ($_SESSION['admin_su'] === false))) { ?>
 	    <script type="text/javascript" src="<?= CONFIG_URL; ?>web/js/gde.chat.js?<?= REVISION; ?>"></script>
     <?php } ?>
 	<script type="text/javascript">var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-3315545-3']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();</script>
