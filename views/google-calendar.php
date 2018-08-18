c2fb0379dc44fa791ee7db70120290d2d58aa6f5
carlosamds <carlos.amds@gmail.com>
2018-08-18 17:09:57 -0300

    Esqueleto da página de opções de inserção no Calendar

--- views/google-calendar.php
+++ views/google-calendar.php
@@ -1,23 +1,56 @@
 <?php
-
-namespace GDE;
-
-define('NO_HTML', true);
+ namespace GDE;
+ define('NO_HTML', true);
 define('TITULO', false);
-
-
-require_once('../common/common.inc.php');
-
-
-$ra = (isset($_GET['ra'])) ? intval($_GET['ra']) : -1;
+ require_once('../common/common.inc.php');
+ $ra = (isset($_GET['ra'])) ? intval($_GET['ra']) : -1;
 $p = (isset($_GET['p'])) ? intval($_GET['p']) : null;
 $n = (isset($_GET['n'])) ? $_GET['n'][0] : 'G';
 
 
-?>
+ ?>
 <html>
+<link rel="stylesheet" href="<?= CONFIG_URL; ?>web/css/gde.css?<?= REVISION; ?>" type="text/css" />
 <head>
 </head>
 <body style="padding: 20px;">
+  <div style="background-color: #FFFFFF; margin: 0 auto; width: auto; text-align: left;">
+    <div id="content_bg">
+      <div style="padding: 20px 15px 20px 15px; width:auto;">
+        <h1 id="anoPeriodo"><?= $Periodo_Selecionado->getNome(false); ?></h1>
+        <br>
+        <h3>Selecione um Calend&aacute;rio existente ou digite algum nome para criar um novo</h3>
+        <br>
+        <br>
+        <div>
+          <div style="float: left;">
+            <select id='select-id-calendario'>
+              <!-- TODO colocar calendarios do usuario aqui -->
+            </select>
+          </div>
+          <div style="float: left; padding-left: 30px;">
+            <input id='input-novo-calendario' type="text" placeholder="Nome do calendário"/>
+          </div>
+        </div>
+
+        <br><br>
+        <div style="clear:both;">
+          <input type="checkbox" id="checkbox-datas-importantes">Adicionar datas do calendário da UNICAMP</input>
+          <ul id='calendario-unicamp'>
+            <!-- TODO colocar datas importantes aqui -->
+          </ul>
+        </div>
+        <br>
+        <br>
+        <script type="text/javascript">
+          function adicionaNoCalendar() {
+            alert("Clicou")
+          }
+        </script>
+
+        <button type="button" onclick="adicionaNoCalendar()">Adicionar hor&aacute;rios no calend&aacute;rio</button>
+      </div>
+    </div>
+  </div>
 </body>
 <html>
