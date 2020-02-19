c4de8773b9fe91cdc33d07b341ef1316b5ff2126
Felipe Guaycuru de C. B. Franco <guaycuru@gmail.com>
2020-02-19 11:34:07 -0300

    Atualiza os termos de uso para constar o uso do Google Calendar

--- views/termos.php
+++ views/termos.php
@@ -3,15 +3,26 @@
 namespace GDE;
 
 define('TITULO', 'Termos de Uso');
-define('NO_DENIAL', true);
+define('NO_LOGIN_CHECK', true);
 
 require_once('../common/common.inc.php');
 
 ?>
-	<h2>Termos de Uso</h2><br />
-	Para usar o GDE, voc&ecirc; deve concordar com o seguinte:<br />
-	O GDE n&atilde;o &eacute; um sistema oficial da Unicamp, e tem por &uacute;nico objetivo auxiliar alunos na vida acad&ecirc;mica.<br />
-	Por ser um sistema criado de forma totalmente volunt&aacute;ria, e estar em fase BETA, o GDE est&aacute; sujeito a erros, BUGs, e at&eacute; mesmo mudan&ccedil;as de c&oacute;digo ao vivo.<br />
-	Todas as informa&ccedil;&otilde;es aqui contidas s&atilde;o de responsabilidade exclusiva de quem as forneceu.<br />
-	Apesar de estar sempre corrigindo falhas encontradas, pe&ccedil;o que por favor me avisem de qualquer erro encontrado, bem como qualquer sugest&atilde;o ou cr&iacute;tica.
+<h2>Termos de Uso</h2><br />
+Para usar o GDE, voc&ecirc; deve concordar com o seguinte:<br />
+O GDE n&atilde;o &eacute; um sistema oficial da Unicamp, e tem por &uacute;nico objetivo auxiliar alunos na vida acad&ecirc;mica.<br />
+Por ser um sistema criado de forma totalmente volunt&aacute;ria, e estar em fase BETA, o GDE est&aacute; sujeito a erros, BUGs, e at&eacute; mesmo mudan&ccedil;as de c&oacute;digo ao vivo.<br />
+Todas as informa&ccedil;&otilde;es aqui contidas s&atilde;o de responsabilidade exclusiva de quem as forneceu.<br />
+Apesar de estar sempre corrigindo falhas encontradas, pe&ccedil;o que por favor me avisem de qualquer erro encontrado, bem como qualquer sugest&atilde;o ou cr&iacute;tica.<br />
+
+<h2>Termos de Privacidade</h2><br />
+Esses termos se aplicam exclusivamente a fun&ccedil;&atilde;o de "Adicionar ao Google Calendar" na aba de matr&iacute;culas.<br /><br />
+AUTORIZA&Ccedil;&Otilde;ES<br />
+Ao logar com sua conta do Google, est&aacute; sendo dado total controle do seu calend&aacute;rio ao sistema do GDE.
+Esse controle é utilizado para mostrar os calend&aacute;rios dispon&iacute;veis para a adi&ccedil;&atilde;o das matr&iacute;culas, para a cria&ccedil;&atilde;o de um calend&aacute;rio novo, caso essa seja a op&ccedil;&atildeo do usu&aacute;rio e finalmente para a cria&ccedil;&atilde;o dos eventos das aulas.<br /><br />
+INFORMA&Ccedil;&Otilde;ES COLETADAS<br />
+O sistema n&atilde;o guarda nenhuma informa&ccedil;&atilde;o do usu&aacute;rio nesse processo, nem calend&aacute;rios, nem eventos, nem sequer a chave de autentica&ccedil;&atilde;o do Google para reuso.
+Por esse motivo, sempre que o usu&aacute;rio desejar usar a fun&ccedil;&atilde;o, ser&aacute; necess&aacute;rio que ele logue novamente.<br /><br />
+TERCEIROS<br />
+Os termos de uso das API's do Google se aplicam aqui. Ent&atilde;o aceitando estes termos, o usu&aacute;rio est&aacute; de acordo com os termos de privacidade das API's do Google.
 <?= $FIM; ?>
