9b938d7c274298ffcbdfda54a39e0eb059b039b2
Felipe Guaycuru de C. B. Franco <guaycuru@gmail.com>
2022-03-15 13:35:52 -0300

    Corrige atualizacao de dados

--- classes/GDE/Dado.php
+++ classes/GDE/Dado.php
@@ -269,125 +269,125 @@ class Dado extends Base {
 		$connection = self::_EM()->getConnection();
 		$Dados = self::Load(self::ID);
 
-		$alunos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos")->fetch();
+		$alunos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos")->fetchOne();
 		if($alunos === false)
 			return false;
-		$Dados->setAlunos($alunos['total']);
+		$Dados->setAlunos($alunos);
 
-		$alunos_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos WHERE nivel IS NOT NULL")->fetch();
+		$alunos_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos WHERE nivel IS NOT NULL")->fetchOne();
 		if($alunos_grad === false)
 			return false;
-		$Dados->setAlunos_Grad($alunos_grad['total']);
+		$Dados->setAlunos_Grad($alunos_grad);
 
-		$alunos_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos WHERE nivel_pos IS NOT NULL")->fetch();
+		$alunos_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos WHERE nivel_pos IS NOT NULL")->fetchOne();
 		if($alunos_pos === false)
 			return false;
-		$Dados->setAlunos_Pos($alunos_pos['total']);
+		$Dados->setAlunos_Pos($alunos_pos);
 
-		$ativos = $connection->executeQuery("SELECT COUNT(DISTINCT ra) AS total FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?)", array($periodo))->fetch();
+		$ativos = $connection->executeQuery("SELECT COUNT(DISTINCT ra) AS total FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?)", array($periodo))->fetchOne();
 		if($ativos === false)
 			return false;
-		$Dados->setAtivos($ativos['total']);
+		$Dados->setAtivos($ativos);
 
-		$ativos_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos WHERE ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?)) AND nivel IS NOT NULL", array($periodo))->fetch();
+		$ativos_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos WHERE ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?)) AND nivel IS NOT NULL", array($periodo))->fetchOne();
 		if($ativos_grad === false)
 			return false;
-		$Dados->setAtivos_Grad($ativos_grad['total']);
+		$Dados->setAtivos_Grad($ativos_grad);
 
-		$ativos_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos WHERE ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?)) AND nivel_pos IS NOT NULL", array($periodo))->fetch();
+		$ativos_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_alunos WHERE ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?)) AND nivel_pos IS NOT NULL", array($periodo))->fetchOne();
 		if($ativos_pos === false)
 			return false;
-		$Dados->setAtivos_Pos($ativos_pos['total']);
+		$Dados->setAtivos_Pos($ativos_pos);
 
-		$oferecimentos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_oferecimentos WHERE id_periodo = ?", array($periodo))->fetch();
+		$oferecimentos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_oferecimentos WHERE id_periodo = ?", array($periodo))->fetchOne();
 		if($oferecimentos === false)
 			return false;
-		$Dados->setOferecimentos($oferecimentos['total']);
+		$Dados->setOferecimentos($oferecimentos);
 
-		$oferecimentos_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_oferecimentos AS O JOIN gde_disciplinas AS D ON (D.id_disciplina = O.id_disciplina) WHERE O.id_periodo = ? AND D.nivel = ?", array($periodo, Disciplina::NIVEL_GRAD))->fetch();
+		$oferecimentos_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_oferecimentos AS O JOIN gde_disciplinas AS D ON (D.id_disciplina = O.id_disciplina) WHERE O.id_periodo = ? AND D.nivel = ?", array($periodo, Disciplina::NIVEL_GRAD))->fetchOne();
 		if($oferecimentos_grad === false)
 			return false;
-		$Dados->setOferecimentos_Grad($oferecimentos_grad['total']);
+		$Dados->setOferecimentos_Grad($oferecimentos_grad);
 
-		$oferecimentos_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_oferecimentos AS O JOIN gde_disciplinas AS D ON (D.id_disciplina = O.id_disciplina) WHERE O.id_periodo = ? AND D.nivel IN (?, ?)", array($periodo, Disciplina::NIVEL_POS, Disciplina::NIVEL_MP))->fetch();
+		$oferecimentos_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_oferecimentos AS O JOIN gde_disciplinas AS D ON (D.id_disciplina = O.id_disciplina) WHERE O.id_periodo = ? AND D.nivel IN (?, ?)", array($periodo, Disciplina::NIVEL_POS, Disciplina::NIVEL_MP))->fetchOne();
 		if($oferecimentos_pos === false)
 			return false;
-		$Dados->setOferecimentos_Pos($oferecimentos_pos['total']);
+		$Dados->setOferecimentos_Pos($oferecimentos_pos);
 
-		$disciplinas = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_disciplinas")->fetch();
+		$disciplinas = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_disciplinas")->fetchOne();
 		if($disciplinas === false)
 			return false;
-		$Dados->setDisciplinas($disciplinas['total']);
+		$Dados->setDisciplinas($disciplinas);
 
-		$disciplinas_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_disciplinas WHERE nivel = ?", Disciplina::NIVEL_GRAD)->fetch();
+		$disciplinas_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_disciplinas WHERE nivel = ?", array(Disciplina::NIVEL_GRAD))->fetchOne();
 		if($disciplinas_grad === false)
 			return false;
-		$Dados->setDisciplinas_Grad($disciplinas_grad['total']);
+		$Dados->setDisciplinas_Grad($disciplinas_grad);
 
-		$disciplinas_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_disciplinas WHERE nivel IN (?, ?)", Disciplina::$NIVEIS_POS)->fetch();
+		$disciplinas_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_disciplinas WHERE nivel IN (?, ?)", Disciplina::$NIVEIS_POS)->fetchOne();
 		if($disciplinas_pos === false)
 			return false;
-		$Dados->setDisciplinas_Pos($disciplinas_pos['total']);
+		$Dados->setDisciplinas_Pos($disciplinas_pos);
 
-		$professores = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_professores")->fetch();
+		$professores = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_professores")->fetchOne();
 		if($professores === false)
 			return false;
-		$Dados->setProfessores($professores['total']);
+		$Dados->setProfessores($professores);
 
-		$salas = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_salas")->fetch();
+		$salas = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_salas")->fetchOne();
 		if($salas === false)
 			return false;
-		$Dados->setSalas($salas['total']);
+		$Dados->setSalas($salas);
 
-		$usuarios = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE")->fetch();
+		$usuarios = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE")->fetchOne();
 		if($usuarios === false)
 			return false;
-		$Dados->setUsuarios($usuarios['total']);
+		$Dados->setUsuarios($usuarios);
 
-		$usuarios_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_alunos WHERE nivel IS NOT NULL)")->fetch();
+		$usuarios_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_alunos WHERE nivel IS NOT NULL)")->fetchOne();
 		if($usuarios_grad === false)
 			return false;
-		$Dados->setUsuarios_Grad($usuarios_grad['total']);
+		$Dados->setUsuarios_Grad($usuarios_grad);
 
-		$usuarios_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_alunos WHERE nivel_pos IS NOT NULL)")->fetch();
+		$usuarios_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_alunos WHERE nivel_pos IS NOT NULL)")->fetchOne();
 		if($usuarios_pos === false)
 			return false;
-		$Dados->setUsuarios_Pos($usuarios_pos['total']);
+		$Dados->setUsuarios_Pos($usuarios_pos);
 
-		$usuarios_ativos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?))", array($periodo))->fetch();
+		$usuarios_ativos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?))", array($periodo))->fetchOne();
 		if($usuarios_ativos === false)
 			return false;
-		$Dados->setUsuarios_Ativos($usuarios_ativos['total']);
+		$Dados->setUsuarios_Ativos($usuarios_ativos);
 
-		$usuarios_ativos_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_alunos WHERE nivel IS NOT NULL) AND ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?))", array($periodo))->fetch();
+		$usuarios_ativos_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_alunos WHERE nivel IS NOT NULL) AND ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?))", array($periodo))->fetchOne();
 		if($usuarios_ativos_grad === false)
 			return false;
-		$Dados->setUsuarios_Ativos_Grad($usuarios_ativos_grad['total']);
+		$Dados->setUsuarios_Ativos_Grad($usuarios_ativos_grad);
 
-		$usuarios_ativos_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_alunos WHERE nivel_pos IS NOT NULL) AND ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?))", array($periodo))->fetch();
+		$usuarios_ativos_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ra IN (SELECT ra FROM gde_alunos WHERE nivel_pos IS NOT NULL) AND ra IN (SELECT ra FROM gde_r_alunos_oferecimentos WHERE id_oferecimento IN (SELECT id_oferecimento FROM gde_oferecimentos WHERE id_periodo = ?))", array($periodo))->fetchOne();
 		if($usuarios_ativos_pos === false)
 			return false;
-		$Dados->setUsuarios_Ativos_Pos($usuarios_ativos_pos['total']);
+		$Dados->setUsuarios_Ativos_Pos($usuarios_ativos_pos);
 
-		$usuarios_acesso = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ultimo_acesso >= ?", array($seis_meses))->fetch();
+		$usuarios_acesso = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ultimo_acesso >= ?", array($seis_meses))->fetchOne();
 		if($usuarios_acesso === false)
 			return false;
-		$Dados->setUsuarios_Acesso($usuarios_acesso['total']);
+		$Dados->setUsuarios_Acesso($usuarios_acesso);
 
-		$usuarios_acesso_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ultimo_acesso >= ? AND ra IN (SELECT ra FROM gde_alunos WHERE nivel IS NOT NULL)", array($seis_meses))->fetch();
+		$usuarios_acesso_grad = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ultimo_acesso >= ? AND ra IN (SELECT ra FROM gde_alunos WHERE nivel IS NOT NULL)", array($seis_meses))->fetchOne();
 		if($usuarios_acesso_grad === false)
 			return false;
-		$Dados->setUsuarios_Acesso_Grad($usuarios_acesso_grad['total']);
+		$Dados->setUsuarios_Acesso_Grad($usuarios_acesso_grad);
 
-		$usuarios_acesso_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ultimo_acesso >= ? AND ra IN (SELECT ra FROM gde_alunos WHERE nivel_pos IS NOT NULL)", array($seis_meses))->fetch();
+		$usuarios_acesso_pos = $connection->executeQuery("SELECT COUNT(*) AS total FROM gde_usuarios WHERE ativo = TRUE AND ultimo_acesso >= ? AND ra IN (SELECT ra FROM gde_alunos WHERE nivel_pos IS NOT NULL)", array($seis_meses))->fetchOne();
 		if($usuarios_acesso_pos === false)
 			return false;
-		$Dados->setUsuarios_Acesso_Pos($usuarios_acesso_pos['total']);
+		$Dados->setUsuarios_Acesso_Pos($usuarios_acesso_pos);
 
-		$usuarios_atividade = $connection->executeQuery("SELECT COUNT(DISTINCT id_origem) AS total FROM gde_acontecimentos WHERE data >= ?", array($seis_meses))->fetch();
+		$usuarios_atividade = $connection->executeQuery("SELECT COUNT(DISTINCT id_origem) AS total FROM gde_acontecimentos WHERE data >= ?", array($seis_meses))->fetchOne();
 		if($usuarios_atividade === false)
 			return false;
-		$Dados->setUsuarios_Atividade($usuarios_atividade['total']);
+		$Dados->setUsuarios_Atividade($usuarios_atividade);
 
 		$Dados->setUltima_Atualizacao();
 
